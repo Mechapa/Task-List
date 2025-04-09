70db2cd7da9e21da3df7c7f1a88042b76b918f69
”Mechapa” <mechapamachito@gmail.com>
2025-04-09 22:48:13 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
-import styles from './App.module.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import TaskPage from './pages/TaskPage';
-import DeletedTaskPage from './pages/DeletedTaskPage';
-=======
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
->>>>>>> main
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
-      <Router>
-        <nav className={styles.nav}>
-          <Link className={styles.link} to="/">Активные задачи</Link>
-          <Link className={styles.link} to="/deleted">Удаленные задачи</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<TaskPage/>}></Route>
-          <Route path="/deleted" element={<DeletedTaskPage/>}></Route>
-        </Routes>
-      </Router>
-=======
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -47,7 +29,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
->>>>>>> main
     </>
   )
 }
