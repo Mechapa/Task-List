78cbedd6366c43e616536a59dc4de43ab7dd21f2
Mechapa <99400658+Mechapa@users.noreply.github.com>
2025-04-09 21:38:19 +0300

    Revert "test commit"

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import taskReducer from './taskSlise';
-const store = configureStore({
-  reducer: {
-    tasks: taskReducer,
-  }
-});
-
-export default store
